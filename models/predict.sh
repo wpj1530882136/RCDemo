10b074081817856395430dbd47df757cd321aa40
wangpeijian <laingwei@trio.ai>
2018-05-20 13:49:55 +0800

    minor

--- models/predict.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#rm -rf models/* tmp/*
-CUDA_VISIBLE_DEVICES=4 nohup  python run.py --predict\
-    --algo MLSTM\
-    --g 4\
-    --test_files /data1/wangpeijian/test1set/preprocessed/zhidao.test1.json > log/predict.log 2>&1 &
